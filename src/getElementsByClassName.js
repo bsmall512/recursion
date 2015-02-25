2051889e66f388013395799053411fe17ce72179
Brian Small <brian.m.small@gmail.com>
2015-02-25 14:47:12 -0600

    makersquare:master bsmall512:master

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,31 @@
 var getElementsByClassName = function(className
 ){
   // your code here
+   var result = [],
+            body = document.body;
+        // Traversing the DOM using a IIFE passing it
+        // the body element and the sorting function
+        (function traverseDOM (node, fn) {
+            // We only check for a match, if we have
+            // an ELEMENT_NODE.
+            if (node.nodeType === 1) {
+                fn(node, className);
+            }
+            node = node.firstChild;
+            // While the current node still have child nodes
+            // we iterate over them.
+            while (node) {
+                // We traverse any potential child nodes of this node.
+                traverseDOM(node, fn);
+                node = node.nextSibling;
+            }
+        }(body, function (elem, cName) {
+            // If we have a match push it to the results array.
+            // Some browsers do not support node.className,
+            // hence the node.getAttribute('class')
+            if ((' ' + (elem.className || elem.getAttribute('class')) + ' ').indexOf(' ' + cName + ' ') >= 0) {
+                result.push(elem);
+            }
+        }));
+        return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,54 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // is it an array, string, or value
+  console.log(obj);
+
+    if (obj === null){
+    return 'null';
+  }
+
+  else if (typeof obj === 'boolean'){
+    if (obj) return 'true'
+    else return 'false';
+  }
+  else if (Array.isArray(obj))
+    { var myArray = '['; 
+        for (var i in obj)
+          {
+            if (myArray != '[')
+              {myArray += ',' + stringifyJSON(obj[i]); }
+            else
+              {myArray += stringifyJSON(obj[i]);}
+          }
+        
+    
+        myArray += ']';
+        return myArray;
+    }
+  else if(typeof obj === "object")
+    { 
+        
+        var myKeys = Object.keys(obj);
+        var myObj = '{';
+
+        for(var i = 0; i < myKeys.length; i++)
+        {
+            if (myKeys[i] == 'undefined' || myKeys[i] == 'function') { return '{}' }
+            else if (myObj != '{'){
+                myObj += ',' + stringifyJSON(myKeys[i]) + ':' + stringifyJSON(obj[myKeys[i]]);
+            }
+            else{
+                myObj += (stringifyJSON(myKeys[i]) + ':' + stringifyJSON(obj[myKeys[i]]));
+            }
+
+        }
+        myObj += '}';
+        return myObj;
+    }
+  else if(typeof obj === 'string')
+    { return '"' + obj + '"' }
+  else
+    return obj.toString();
+
 };
